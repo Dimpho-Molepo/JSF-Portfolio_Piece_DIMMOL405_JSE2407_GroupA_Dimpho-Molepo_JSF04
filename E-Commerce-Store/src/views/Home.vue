86c702d0b7d405e27c30ca1b9eed95e67bc035bb
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-04 16:41:40 +0200

    Added the Home page

---
+++ E-Commerce-Store/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class=" bg-gray-200">
+    <FilterSort />
+    <ProductList />
+  </main>
+</template>
+
+<script>
+import FilterSort from '@/components/FilterSort.vue'
+import ProductList from '@/components/product/ProductList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    FilterSort,
+    ProductList,
+  }
+}
+</script>
