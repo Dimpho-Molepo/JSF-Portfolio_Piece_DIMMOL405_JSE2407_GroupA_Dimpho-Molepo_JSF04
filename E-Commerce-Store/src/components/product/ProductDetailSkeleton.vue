4a5ca53d191d17d51e355ac8407e10250d9b98fe
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-04 17:07:19 +0200

    Added a ProductDetailSkeleton

---
+++ E-Commerce-Store/src/components/product/ProductDetailSkeleton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-6 sm:mt-8 lg:flex lg:items-start lg:max-w-6xl xl:max-w-7xl mx-auto animate-pulse">
+    <div class="w-full h-full items-center justify-center flex lg:w-2/5 mb-8 lg:mb-0">
+      <div class="w-[50%] h-96 bg-gray-300 rounded"></div>
+    </div>
+    <div class="w-full lg:w-3/5 lg:pl-8 space-y-4">
+      <div class="h-10 bg-gray-300 rounded w-3/4"></div>
+      <div class="flex items-center gap-2">
+        <div class="w-24 h-4 bg-gray-300 rounded"></div>
+        <div class="w-16 h-4 bg-gray-300 rounded"></div>
+      </div>
+      <div class="w-20 h-6 bg-gray-300 rounded"></div>
+      <div class="w-24 h-8 bg-gray-300 rounded"></div>
+      <div class="w-32 h-10 bg-gray-300 rounded"></div>
+      <div class="h-6 bg-gray-300 rounded w-1/4"></div>
+      <div class="h-4 bg-gray-300 rounded"></div>
+      <div class="h-4 bg-gray-300 rounded"></div>
+      <div class="h-4 bg-gray-300 rounded w-3/4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetailSkeleton'
+}
+</script>
