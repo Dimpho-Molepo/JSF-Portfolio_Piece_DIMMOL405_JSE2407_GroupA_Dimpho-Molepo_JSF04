a96132d9da0bd49b67de47e1eba9e4524cee9bb6
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-13 18:21:27 +0200

    Modified the router to include authentication checks

--- E-Commerce-Store/src/router/index.js
+++ E-Commerce-Store/src/router/index.js
@@ -2,13 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import ProductDetailView from '@/views/ProductDetailView.vue'
 import Login from '@/views/Login.vue'
+import { useAuthenticationStore } from '@/stores/loginAuthenticate'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
     {
@@ -17,12 +18,33 @@ const router = createRouter({
       component: ProductDetailView
     },
     {
-      path: '/Login',
+      path: '/login',
       name: 'Login',
-      component: Login
-    }
-    
+      component: Login,
+      // meta: { requiresAuth: true }
+    },
+    // {
+    //   path: '/wishlist',
+    //   component: Wishlist,
+    //   meta: { requiresAuth: true }
+    // },
+    // {
+    //   path: '/cart',
+    //   component: Cart,
+    //   meta: { requiresAuth: true }
+    // },
   ]
+})
+
+
+router.beforeEach((to, from, next) => {
+  const login = useAuthenticationStore()
+  
+  if (to.matched.some(record => record.meta.requiresAuth) && !login.isAuthenticated) {
+    next({ name: 'Login', query: { redirect: to.fullPath } })
+  } else {
+    next()
+  }
 })
 
 export default router
