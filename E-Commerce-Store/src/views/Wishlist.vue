f0a59eda9b9071af0d7cf5d0e19d48e048c2d54b
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-17 21:46:37 +0200

    Added the wishlist page

---
+++ E-Commerce-Store/src/views/Wishlist.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mx-auto p-6 bg-gray-100">
+    <h2 class="text-3xl font-bold text-center mb-8">Wishlist</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <ProductCard
+        v-for="product in wishlistStore.products"
+        :key="product.id"
+        v-bind="product"
+      />
+    </div>
+    <button @click="clearWishlist"> clear</button>
+  </div>
+</template>
+
+<script setup>
+import { useWishlistStore } from '../stores/wishlistStore'
+import ProductCard from '@/components/product/ProductCard.vue'
+
+const wishlistStore = useWishlistStore()
+
+const clearWishlist = () => {
+  wishlistStore.clearWishlist()
+}
+</script>
