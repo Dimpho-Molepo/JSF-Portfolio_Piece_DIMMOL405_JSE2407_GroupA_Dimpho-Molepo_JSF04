c8c83abbffa177a557ae105df8cdb83664b3ce4c
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-18 17:52:05 +0200

    Import the filterSort component

--- E-Commerce-Store/src/views/Wishlist.vue
+++ E-Commerce-Store/src/views/Wishlist.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="container mx-auto p-6 bg-gray-100">
     <h2 class="text-3xl font-bold text-center mb-8">Wishlist</h2>
+    <FilterSort
+      :categories="wishlistStore.categories"
+      v-model:selectedSort="wishlistStore.selectedSort"
+      v-model:selectedCategory="wishlistStore.selectedCategory"
+      @reset="resetFilters"
+    />
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
       <ProductCard
-        v-for="product in wishlistStore.products"
+        v-for="product in wishlistStore.filteredSortedProducts"
         :key="product.id"
         v-bind="product"
       />
     </div>
-    <button @click="clearWishlist"> clear</button>
+    <button @click="clearWishlist" class="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg">
+      Clear Wishlist
+    </button>
   </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import { useWishlistStore } from '../stores/wishlistStore'
 import ProductCard from '@/components/product/ProductCard.vue'
+import FilterSort from '@/components/FilterSort.vue'
 
 const wishlistStore = useWishlistStore()
 
 const clearWishlist = () => {
   wishlistStore.clearWishlist()
 }
+
+const resetFilters = () => {
+  wishlistStore.selectedCategory = ''
+  wishlistStore.selectedSort = ''
+}
+
+onMounted(() => {
+  wishlistStore.loadFromLocalStorage()
+})
 </script>
