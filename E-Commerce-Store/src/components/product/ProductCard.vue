1b47e00a6e6cdf3f937097332151b409ab2c9997
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-20 15:05:05 +0200

    Added the dark class and toasts for event notification

--- E-Commerce-Store/src/components/product/ProductCard.vue
+++ E-Commerce-Store/src/components/product/ProductCard.vue
@@ -1,43 +1,42 @@
 <template>
   <div
-    class="flex flex-col max-h-[130rem] max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
+    class="flex flex-col max-h-[130rem] max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-[#f8f9fa] dark:bg-gray-700 border border-slate-300 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
   >
-    <div class="relative">
-      <span
-        class="absolute top-4 left-4 bg-blue-200 text-blue-700 text-xs font-bold py-1 px-2 rounded-lg ring-blue-700/10"
-      >
-        {{ category }}
-      </span>
+    <div class="flex justify-between top-3 p-2">
+      <button @click="toggleWishlist" class="focus:outline-none">
+        <svg
+          class="h-6 w-6"
+          :class="{ 'text-[#da1e37]': isWished, 'text-gray-400': !isWished }"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            v-if="isWished"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+          />
+          <path
+            v-else
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+          />
+        </svg>
+      </button>
+
+      <div class="">
+        <span
+          class="bg-blue-200 text-blue-700 text-xs font-bold py-1 px-2 rounded-lg ring-blue-700/10"
+        >
+          {{ category }}
+        </span>
+      </div>
     </div>
 
-    <button
-      @click="toggleWishlist"
-      class="absolute top-2 right-2 p-2 rounded-full focus:outline-none"
-      :class="{ 'bg-blue-500': isWished, 'bg-gray-300': !isWished }"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          v-if="isWished"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-        />
-        <path
-          v-else
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-        />
-      </svg>
-    </button>
     <img
       @click="handleClick"
       class="object-contain h-48 mt-3 cursor-pointer"
@@ -49,22 +48,22 @@
         <header class="mb-2 flex-2">
           <h2 class="text-lg line-clamp-2 font-extrabold leading-snug">
             <div
-              class="text-slate-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+              class="text-slate-600 dark:text-slate-50 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
             >
               {{ title }}
             </div>
           </h2>
         </header>
         <Rating :rating="rating.rate" :count="rating.count" />
-        <div class="text-base line-clamp-2 font-extrabold text-slate-500 leading-snug">
+        <div class="text-base line-clamp-2 font-extrabold text-slate-500 dark:text-slate-50 leading-snug">
           <h2>R{{ price }}</h2>
         </div>
       </div>
 
-      <div class="flex mt-3">
+      <div class="flex flex-col gap-2 mt-3">
         <button
           @click="addToCart"
-          class="flex items-center justify-between w-full px-4 py-2 bg-blue-400 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-200"
+          class="flex items-center justify-between w-full px-4 py-2 hover:bg-[#0408f7] text-white font-semibold rounded-lg shadow-md bg-[#03045e] focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-200"
         >
           Add to cart
           <svg
@@ -87,22 +86,43 @@
 
         <button
           @click="addToComparison"
-          class="flex-1 px-4 py-2 bg-green-400 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-200"
+          class="flex items-center justify-between w-full px-4 py-2 hover:bg-green-500 text-white font-semibold rounded-lg shadow-md bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-200"
         >
           Compare
+          <svg
+            class="w-5 h-5 ml-2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#ffffff"
+            stroke="#ffffff"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M2 4h9v1H3v15h8v1H2zm10 19h1V2h-1zM8.283 10.283l-.566-.566L4.934 12.5l2.783 2.783.566-.566L6.566 13H11v-1H6.566zM14 12h4.08l-1.54-1.54.92-.92 2.96 2.96-2.96 2.96-.92-.92L18.08 13H14v8h9V4h-9z"
+              ></path>  
+              <path fill="none" d="M0 0h24v24H0z"></path>
+            </g>
+          </svg>
         </button>
       </div>
     </div>
   </div>
+
+  <Toast v-if="showToast" :type="toastType" @closed="closeToast">
+    {{ toastMessage }}
+  </Toast>
 </template>
 
 <script setup>
 import Rating from '../Rating.vue'
 import { useRouter } from 'vue-router'
-import { defineProps, computed } from 'vue'
+import { defineProps, computed, ref } from 'vue'
 import { useCartStore } from '@/stores/cartStore.js'
 import { useComparisonStore } from '@/stores/comparisonStore.js'
 import { useWishlistStore } from '@/stores/wishlistStore.js'
+import Toast from '../Toast.vue'
 
 /**
  * @module ProductCard
@@ -144,11 +164,25 @@ const comparisonStore = useComparisonStore()
 const router = useRouter()
 const cartStore = useCartStore()
 const wishlistStore = useWishlistStore()
+const showToast = ref(false)
+const toastType = ref('success')
+const toastMessage = ref('')
+
 /**
  * Navigates to the product detail page
  */
 const handleClick = () => {
   router.push(`/products/${props.id}`)
+}
+
+const showToastNotification = (type, message) => {
+  toastType.value = type
+  toastMessage.value = message
+  showToast.value = true
+}
+
+const closeToast = () => {
+  showToast.value = false
 }
 
 const addToCart = () => {
@@ -158,7 +192,9 @@ const addToCart = () => {
     price: props.price,
     image: props.image
   })
+  showToastNotification('success', 'Product added to cart')
 }
+
 
 const addToComparison = () => {
   comparisonStore.addProduct({
@@ -169,6 +205,7 @@ const addToComparison = () => {
     description: props.description,
     rating: props.rating
   })
+  showToastNotification('success', 'Product added to comparison')
 }
 const isWished = computed(() => {
   return wishlistStore.products.some((product) => product.id === props.id)
@@ -177,6 +214,7 @@ const isWished = computed(() => {
 const toggleWishlist = () => {
   if (isWished.value) {
     wishlistStore.removeProduct(props.id)
+    showToastNotification('warning', 'Product removed from wishlist')
   } else {
     wishlistStore.addToWishlist({
       id: props.id,
@@ -187,6 +225,7 @@ const toggleWishlist = () => {
       rating: props.rating,
       category: props.category
     })
+    showToastNotification('success', 'Product added to wishlist')
   }
   console.log(`Product ${isWished.value ? 'removed from' : 'added to'} wishlist`)
 }
