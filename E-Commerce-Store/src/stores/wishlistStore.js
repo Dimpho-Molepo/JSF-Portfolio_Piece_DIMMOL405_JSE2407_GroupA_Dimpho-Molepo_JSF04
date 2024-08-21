f14361817d45824923a8126da5c714214c9a479f
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-21 10:57:05 +0200

    Added JsDocs comments

--- E-Commerce-Store/src/stores/wishlistStore.js
+++ E-Commerce-Store/src/stores/wishlistStore.js
@@ -3,37 +3,78 @@ import { ref, computed } from 'vue'
 import { useProductStore } from '@/stores/productStore'
 
 /**
- * Description placeholder
+ * Wishlist store
  *
- * @type {*}
+ * @module WishlistStore
+ * @description Manages the wishlist functionality
  */
 export const useWishlistStore = defineStore('wishlist', () => {
+  /**
+   * @type {ref<products[]>}
+   * @description The list of products in the wishlist
+   */
   const products = ref([])
+
+  /**
+   * @type {import('@/stores/productStore').default}
+   * @description The product store instance
+   */
   const productStore = useProductStore()
+
+  /**
+   * @type {ref<string>}
+   * @description The selected category for filtering
+   */
   const selectedCategory = ref('')
+
+  /**
+   * @type {ref<string>}
+   * @description The selected sorting method
+   */
   const selectedSort = ref('')
 
+  /**
+   * Adds a product to the wishlist
+   * @param {product} product - The product to add
+   */
   const addToWishlist = (product) => {
     products.value.push({ ...product })
     saveToLocalStorage()
   }
 
+  /**
+   * Removes a product from the wishlist
+   * @param {number} productId - The ID of the product to remove
+   */
   const removeProduct = (productId) => {
     products.value = products.value.filter((item) => item.id !== productId)
     saveToLocalStorage()
   }
 
+  /**
+   * Clears the entire wishlist
+   */
   const clearWishlist = () => {
     products.value = []
     saveToLocalStorage()
   }
 
+  /**
+   * @type {computed<number>}
+   * @description The total number of products in the wishlist
+   */
   const totalProducts = computed(() => products.value.length)
 
+  /**
+   * Saves the wishlist to local storage
+   */
   const saveToLocalStorage = () => {
     localStorage.setItem(`Wishlist`, JSON.stringify(products.value))
   }
 
+  /**
+   * Loads the wishlist from local storage
+   */
   const loadFromLocalStorage = () => {
     const savedWishlist = localStorage.getItem(`Wishlist`)
     if (savedWishlist) {
