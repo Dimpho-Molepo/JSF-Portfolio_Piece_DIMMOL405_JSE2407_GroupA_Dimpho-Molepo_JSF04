b450ea95b9796c153a9b4bd5b56e952fec3531c6
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-08-21 15:19:30 +0200

    Refacted the code to autofill the users info

--- E-Commerce-Store/src/stores/loginAuthenticate.js
+++ E-Commerce-Store/src/stores/loginAuthenticate.js
@@ -3,14 +3,15 @@ import { jwtDecode } from 'jwt-decode'
 import { ref, computed } from 'vue'
 
 export const useAuthenticationStore = defineStore('loginAuthentication', () => {
-  const decodedToken = ref(localStorage.getItem('token') || null)
-  const userLogin = ref(false)
+  const token = ref(localStorage.getItem('token') || null)
+  const decodedToken = ref(token.value ? jwtDecode(token.value) : null)
+  const userLogin = ref(!!token.value)
   const user = ref(null)
   const loading = ref(false)
   const error = ref(null)
   const returnTo = ref('/')
 
-  const isAuthenticated = computed(() => !!decodedToken.value)
+  const isAuthenticated = computed(() => !!token.value)
 
   const login = async (username, password) => {
     loading.value = true
@@ -27,21 +28,37 @@ export const useAuthenticationStore = defineStore('loginAuthentication', () => {
       const data = await response.json()
 
       if (data.token) {
-        const token = data.token
-        decodedToken.value = jwtDecode(token)
+        token.value = data.token
+        decodedToken.value = jwtDecode(token.value)
         userLogin.value = true
-        user.value = decodedToken.value.user
-        localStorage.setItem('token', token)
+        localStorage.setItem('token', token.value)
+        await fetchUserInfo()
         loading.value = false
         return true
       } else {
         throw new Error('Login failed. No token received.')
       }
-    } catch (error) {
-      console.error('Login error:', error)
+    } catch (err) {
+      console.error('Login error:', err)
       error.value = 'Login failed. Please check your credentials.'
       loading.value = false
       return false
+    }
+  }
+
+  const fetchUserInfo = async () => {
+    if (!decodedToken.value) return
+
+    const userId = decodedToken.value.sub
+    try {
+      const response = await fetch(`https://fakestoreapi.com/users/${userId}`)
+      const data = await response.json()
+
+      userInfo.value.name = `${data.name.firstname} ${data.name.lastname}`
+      userInfo.value.address = `${data.address.street}, ${data.address.city}, ${data.address.zipcode}`
+      userInfo.value.email = data.email
+    } catch (error) {
+      console.error('Error fetching user information:', error)
     }
   }
 
@@ -65,6 +82,8 @@ export const useAuthenticationStore = defineStore('loginAuthentication', () => {
     error,
     loading,
     setReturnTo,
-    returnTo
+    returnTo,
+    user,
+    fetchUserInfo
   }
 })
